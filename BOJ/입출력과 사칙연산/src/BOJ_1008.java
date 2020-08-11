1c71a40ec5ab1b0d7f5a5786635e4e4f44b5cf95
kyunghoonkim <kkh3@cau.ac.kr>
2020-08-11 20:18:05 +0900

    주석 추가

--- BOJ/입출력과 사칙연산/src/BOJ_1008.java
+++ BOJ/입출력과 사칙연산/src/BOJ_1008.java
@@ -1,3 +1,7 @@
+/*
+ 두 정수 A와 B를 입력받은 다음, A/B를 출력하는 프로그램을 작성하시오.
+
+ */
 import java.util.*;
 
 public class BOJ_1008 {
--- BOJ/입출력과 사칙연산/src/BOJ_2557.java
+++ BOJ/입출력과 사칙연산/src/BOJ_2557.java
@@ -1,4 +1,6 @@
-
+/*
+ Hello World!를 출력하시오.
+ */
 
 public class BOJ_2557 {
     public static void main(String args[]){
