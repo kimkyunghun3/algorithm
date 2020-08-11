106b8025f43c93cbfd48d923058a5c486b58d475
kyunghoonkim <kkh3@cau.ac.kr>
2020-08-11 20:40:01 +0900

    BOJ 1008 두 정수의 A와 B를 입력받은 다음, A/B를 출력하는 프로그램

--- BOJ/입출력과 사칙연산/src/BOJ_10998.java
+++ BOJ/입출력과 사칙연산/src/BOJ_10998.java
@@ -1,15 +1,15 @@
 /*
  두 정수 A와 B를 입력받은 다음, A/B를 출력하는 프로그램을 작성하시오.
+
  */
+import java.util.*;
 
-import java.util.Scanner;
-
-public class BOJ_1008 {
+public class BOJ_10998 {
     public static void main(String args[]){
         Scanner scanner = new Scanner(System.in);
         int a,b;
         a = scanner.nextInt();
         b = scanner.nextInt();
-        System.out.println((double)a/b);
+        System.out.println(a*b);
     }
 }
