5b6d4167016f50e51c4f4f7e56535f4b1d76d7d6
kyunghoonkim <kkh3@cau.ac.kr>
2020-08-11 20:15:38 +0900

    BOJ 1000, BOJ 1008, BOJ 2557, BOJ 10869 풀이

---
+++ BOJ/입출력과 사칙연산/src/BOJ_1008.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class BOJ_1008 {
+    public static void main(String args[]){
+        Scanner scanner = new Scanner(System.in);
+        int a,b;
+        a = scanner.nextInt();
+        b = scanner.nextInt();
+        System.out.println(a*b);
+    }
+}
